aaf5136b3d7a70d285bbad5a080ac00a0d4770be
gf <2545706530@qq.com>
2018-04-28 16:15:52 +0800

    Initial Commit

---
+++ jpushTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jpushTest
+//
+//  Created by 飞鱼2100 on 2018/4/28.
+//  Copyright © 2018年 feiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jpushTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jpushTest
+//
+//  Created by 飞鱼2100 on 2018/4/28.
+//  Copyright © 2018年 feiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
